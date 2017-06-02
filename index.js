94e4a91b53d7d16c185b25899a388773decc7468
Christian Landgren <christian@landgren.nu>
2017-06-03 01:37:09 +0200

    add support for classes and components

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const render = require('./lib/render')
 const createElement = require('./lib/createElement')
 const createStore = require('./lib/createStore')
 
+class Component {
+}
+
 module.exports = {
   render,
   createElement,
-  createStore
+  createStore,
+  Component
 }
