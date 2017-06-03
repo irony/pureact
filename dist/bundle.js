b1054f9c3a370a6f598edc3a7c507d57daa5c51b
Christian Landgren <christian@landgren.nu>
2017-06-03 15:01:53 +0200

    don't use dist version

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,15 +3,28 @@
 },{}],2:[function(require,module,exports){
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var render = require('./lib/render');
 var createElement = require('./lib/createElement');
 var createStore = require('./lib/createStore');
 
-var Component = function Component() {
-  _classCallCheck(this, Component);
-};
+var Component = function () {
+  function Component() {
+    _classCallCheck(this, Component);
+  }
+
+  _createClass(Component, [{
+    key: 'setState',
+    value: function setState() {
+      throw new Error('Unsupported. Use props instead of component state');
+    }
+  }]);
+
+  return Component;
+}();
 
 module.exports = {
   render: render,
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const createElement = require('./lib/createElement')
 const createStore = require('./lib/createStore')
 
 class Component {
+  setState () { throw new Error('Unsupported. Use props instead of component state') }
 }
 
 module.exports = {
