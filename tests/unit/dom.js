715d93e7634968357e21aec612726b131289ece4
Christian Landgren <christian@landgren.nu>
2017-06-21 14:21:38 +0200

    fix test

--- tests/unit/dom.js
+++ tests/unit/dom.js
@@ -4,8 +4,8 @@ const oboy = require('oboy')
 oboy((expect, sinon) => {
   it('should render a tag with children', (done) => {
     const nav = { tagName: 'nav', children: [] }
-    const div = { tagName: 'div', className: 'nav', children: [nav] }
-    const node = { appendChild: sinon.spy(), children: [] }
+    const div = { tagName: 'div', className: 'nav', children: [nav], document: sinon.spy() }
+    const node = { tagName: 'img', appendChild: sinon.spy(), children: [] }
     render(div, node)
     expect(node.appendChild).to.be.calledOnce
     done()
