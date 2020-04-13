f0fa7b949f45c7c67e34dfe266b3873864dfd1e4
Christian Landgren <christian@landgren.nu>
2020-04-13 18:32:07 +0200

    use context

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ const createElement = require('./createElement')
 const createStore = require('./createStore')
 const useState = require('./useState')
 const useReducer = require('./useReducer')
+const useContext = require('./useContext')
+//const createContext = require('./createContext')
 
 function Component (props) {
   this.props = props
-  this.setState = () => { throw new Error('Unsupported. Use useState instead') }
+  this.setState = () => { throw new Error('Unsupported. Use hooks/useState instead') }
 }
 module.exports = {
   render,
   createElement,
   createStore,
+  //createContext,
   useState,
   useReducer,
+  useContext,
   Component
 }
