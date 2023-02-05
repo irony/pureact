62c8238f3e21eaa72dcccb7d1820ff40846edb14
Christian Landgren <christian@landgren.nu>
2023-02-05 23:19:14 +0100

    fix: another way of both exporting default and separate imports

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,9 @@
-import render from './render.mjs'
-import createElement from './createElement.mjs'
-import createStore from './createStore.mjs'
-import useState from './useState.mjs'
-import useReducer from './useReducer.mjs'
-import useContext from './useContext.mjs'
+export { default as render } from './render.mjs'
+export { default as createElement } from './createElement.mjs'
+export { default as createStore } from './createStore.mjs'
+export { default as useState } from './useState.mjs'
+export { default as useReducer } from './useReducer.mjs'
+export { default as useContext } from './useContext.mjs'
 
 function Component(props) {
   this.props = props
