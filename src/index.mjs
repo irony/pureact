b753589d22ed2ac0c2ce3b08d44cf3320261f175
Christian Landgren <christian@landgren.nu>
2023-02-05 23:52:27 +0100

    fix: bugfix

--- src/index.mjs
+++ src/index.mjs
@@ -5,6 +5,13 @@ export { default as useState } from './useState.mjs'
 export { default as useReducer } from './useReducer.mjs'
 export { default as useContext } from './useContext.mjs'
 
+import render from './render.mjs'
+import createElement from './createElement.mjs'
+import createStore from './createStore.mjs'
+import useState from './useState.mjs'
+import useReducer from './useReducer.mjs'
+import useContext from './useContext.mjs'
+
 export function Component(props) {
   this.props = props
   this.setState = () => {
